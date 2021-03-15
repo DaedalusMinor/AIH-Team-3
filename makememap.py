274c78c10256b845b0c51a272b748477b623cc10
louiswenz <54404929+louiswenz@users.noreply.github.com>
2021-03-15 19:30:20 -0400

    Update makememap.py

--- makememap.py
+++ makememap.py
@@ -78,5 +78,39 @@ def change_color_to_risk():
                             fill_opacity=1.0,
                             color="#888888").add_to(new_map)
     new_map.save(map_dir)
+def clamp(n, minn, maxn):
+    return max(min(maxn, n), minn)
+
+def change_color_to_speed():
+    new_map = folium.Map(location=start_location,control_scale=True) 
+    speed=0
+    avewalk=0.084
+ 
+    speeddiff=0
+        
+    for i in range(len(locations)):
+        if i == 0:
+            speed=0;
+        elif (times[i]-times[i-1])==0:
+            speed=0;
+        else:
+            coords_1 = [locations[i][0], locations[i][1]]
+            coords_2 = [locations[i-1][0], locations[i-1][1]]
+            distance = h3.point_dist(coords_1,coords_2)
+            speed= abs(distance / (times[i]-times[i-1]))
+        
+        speeddiff=speed*1000/60 - 1.4
+        r=clamp(100+speeddiff*300,0,255)    #grey normal, yellow fast, blue slow
+        g=clamp(100+speeddiff*100,0,255)
+        b=clamp(100-speeddiff*100,0,255)
+        color_tuple = (int(r), int(g), int(b))
+        rgb = rgb_to_hex(color_tuple)
+        folium.CircleMarker(location=[locations[i][0], locations[i][1]],
+                            fill_color=rgb,
+                            fill=True,
+                            popup="Speed: "+str(speed*1000/60)+" m/s",
+                            fill_opacity=1.0,
+                            color="#888888",radius=5).add_to(new_map)
+        new_map.save(map_dir)
 
 change_color_to_risk()
