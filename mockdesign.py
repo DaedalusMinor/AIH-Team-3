576bd5b5b861e4f1a0d8ba2a151c8eef48c8336d
DaedalusMinor <34559192+DaedalusMinor@users.noreply.github.com>
2021-03-22 20:22:49 -0400

    Update mockdesign.py

--- mockdesign.py
+++ mockdesign.py
@@ -1,126 +1,120 @@
-import folium
-import numpy
-import random
-import math
-from math import *
-import h3
+from PyQt5 import QtCore, QtGui, QtWidgets, QtWebEngineWidgets
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
+import os
+import sys
+import makememap
 
-# defining the number of steps 
-n = 200
-
-#creating two array for containing x and y coordinate 
-#of size equals to the number of size and filled up with 0's 
-x = numpy.zeros(n)
-y = numpy.zeros(n)
-locations = []
-start_location=[40.4259,-86.9081]
-m = folium.Map(location=start_location, zoom_start=13)
-at_risk = numpy.random.uniform(low=0.0, high=1.1, size=(n,))
-start_time = 0
-map_dir = "index.html"
-MINUTES_IN_DAY = 1440
-times = list(range(start_time, (start_time + n)))
-
-for i in range(len(times)):
-    noise = random.randint(1,5)
-    times[i] = (times[i] + noise) % MINUTES_IN_DAY
-  
-# filling the coordinates with random variables 
-for i in range(1, n): 
-    val = random.randint(1, 4) 
-    if val == 1: 
-        x[i] = x[i - 1] + 0.001
-        y[i] = y[i - 1] 
-    elif val == 2: 
-        x[i] = x[i - 1] - 0.001
-        y[i] = y[i - 1] 
-    elif val == 3: 
-        x[i] = x[i - 1] 
-        y[i] = y[i - 1] + 0.001
-    else: 
-        x[i] = x[i - 1] 
-        y[i] = y[i - 1] - 0.001
-
-    locations.append([x[i] + start_location[0], y[i] + start_location[1]])
-
-
-def rgb_to_hex(rgb):
-    return '#' + ('%02x%02x%02x' % rgb)
-
-def change_color_to_time():
-    new_map = folium.Map(location=start_location)
-    #folium.PolyLine(locations).add_to(new_map)
-    for i in range(len(locations)):
-        time = times[i]
-        r = 255 - math.trunc(255 * (time / MINUTES_IN_DAY))
-        color_tuple = (r, r, r)
-        rgb = rgb_to_hex(color_tuple)
-        folium.CircleMarker(location=[locations[i][0], locations[i][1]],
-                            fill_color=rgb,
-                            fill=True,
-                            radius=5,
-                            popup="Location: " + str(locations[i][0]) + ", " + str(locations[i][1]) + "\nTime:" + str(time),
-                            fill_opacity=1.0,
-                            color="#888888").add_to(new_map)
-    new_map.save(map_dir)
-    
-def change_color_to_risk():
-    new_map = folium.Map(location=start_location)
-    #folium.PolyLine(locations).add_to(new_map)
-    for i in range(len(locations)):
-        time = times[i]
-        risk = math.trunc(at_risk[i])
-        color_tuple = (255 * (risk), 255 * (1 - risk), 0)
-        rgb = rgb_to_hex(color_tuple)
-        folium.CircleMarker(location=[locations[i][0], locations[i][1]],
-                            fill_color=rgb,
-                            fill=True,
-                            radius=5,
-                            popup="Location: " + str(locations[i][0]) + ", " + str(locations[i][1]) + "\nTime:" + str(time),
-                            fill_opacity=1.0,
-                            color="#888888").add_to(new_map)
-    new_map.save(map_dir)
-def clamp(n, minn, maxn):
-    return max(min(maxn, n), minn)
-
-def change_color_to_speed():
-    new_map = folium.Map(location=start_location,control_scale=True) 
-    speed=0
-    avewalk=0.084
- 
-    speeddiff=0
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        makememap.change_color_to_time()
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+            
+        MainWindow.setEnabled(True)
+        MainWindow.resize(955, 824)
+        MainWindow.setMouseTracking(False)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
         
-    for i in range(len(locations)):
+        self.verticalLayoutWidget = QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(30, 20, 531, 521))
+        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+
+        # Add Map viewer to Verical Layout
+        self.webEngineView = QtWebEngineWidgets.QWebEngineView(self.centralwidget)
+        url = QtCore.QUrl('http://127.0.0.1:8050/')
+        ##QtCore.QUrl().fromLocalFile(os.path.split(os.path.abspath(__file__))[0]+r'index.html'
+        self.webEngineView.load(url)
+        self.verticalLayout.addWidget(self.webEngineView)
+
+        # Pressing button shows a dialog box
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QtCore.QRect(600, 100, 181, 31))
+        
+
+        self.horizontalSlider = QSlider(self.centralwidget)
+        self.horizontalSlider.setObjectName(u"horizontalSlider")
+        self.horizontalSlider.setGeometry(QtCore.QRect(590, 40, 261, 31))
+        self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.pushButton_2 = QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QtCore.QRect(600, 180, 181, 31))
+        self.comboBox = QComboBox(self.centralwidget)
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.setObjectName(u"comboBox")
+        self.comboBox.setGeometry(QtCore.QRect(600, 250, 181, 31))
+        self.pushButton_3 = QPushButton(self.centralwidget)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButton_3.setGeometry(QtCore.QRect(640, 520, 112, 32))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.pushButton.raise_()
+        self.verticalLayoutWidget.raise_()
+        self.horizontalSlider.raise_()
+        self.pushButton_2.raise_()
+        self.comboBox.raise_()
+        self.pushButton_3.raise_()
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 955, 22))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        QWidget.setTabOrder(self.pushButton_2, self.comboBox)
+        QWidget.setTabOrder(self.comboBox, self.horizontalSlider)
+        QWidget.setTabOrder(self.horizontalSlider, self.pushButton_3)
+        QWidget.setTabOrder(self.pushButton_3, self.pushButton)
+
+        self.retranslateUi(MainWindow)
+        self.pushButton.clicked.connect(self.show_dialog)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def show_dialog(self):
+        msg = QMessageBox()
+        msg.setWindowTitle("Confirmation")
+        msg.setText("Are you sure?")
+        msg.setStandardButtons(QMessageBox.Yes|QMessageBox.No)
+
+        x = msg.exec_()
+
+    def selectionchange(self, i):
+        ## S T R
         if i == 0:
-            speed=0
-        elif (times[i]-times[i-1])==0:
-            speed=0
-        else:
-            #coords_1 = [locations[i][0], locations[i][1]]
-            #coords_2 = [locations[i-1][0], locations[i-1][1]]
-            #distance = h3.point_dist(coords_1,coords_2)
-            R = 6373.0
-            lat1 = radians(locations[i][0])
-            lon1 = radians(locations[i][1])
-            lat2 = radians(locations[i-1][0])
-            lon2 = radians(locations[i-1][1])
-            dlon = lon2 - lon1
-            dlat = lat2 - lat1
-            a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-            c = 2 * atan2(sqrt(a), sqrt(1 - a))
-            distance = R * c
-            speed = abs(distance / (times[i]-times[i-1]))
+            makememap.change_color_to_time()
+        if i == 1:
+            makememap.change_color_to_speed()
+        if i == 2:
+            makememap.change_color_to_risk()
+    
+        self.webEngineView.reload()
         
-        speeddiff=speed*1000/60 - 1.4
-        r=clamp(100+speeddiff*300,0,255)    #grey normal, yellow fast, blue slow
-        g=clamp(100+speeddiff*100,0,255)
-        b=clamp(100-speeddiff*100,0,255)
-        color_tuple = (int(r), int(g), int(b))
-        rgb = rgb_to_hex(color_tuple)
-        folium.CircleMarker(location=[locations[i][0], locations[i][1]],
-                            fill_color=rgb,
-                            fill=True,
-                            popup="Speed: "+str(speed*1000/60)+" m/s",
-                            fill_opacity=1.0,
-                            color="#888888",radius=5).add_to(new_map)
-        new_map.save(map_dir)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QtCore.QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.pushButton.setText(QtCore.QCoreApplication.translate("MainWindow", u"Toggle Outlier Selection", None))
+        self.pushButton_2.setText(QtCore.QCoreApplication.translate("MainWindow", u"Toggle Trajectory", None))
+        self.comboBox.setItemText(0, QtCore.QCoreApplication.translate("MainWindow", u"Time", None))
+        self.comboBox.setItemText(1, QtCore.QCoreApplication.translate("MainWindow", u"Speed", None))
+        self.comboBox.setItemText(2, QtCore.QCoreApplication.translate("MainWindow", u"Risk", None))
+        self.comboBox.currentIndexChanged.connect(self.selectionchange)
+
+        self.pushButton_3.setText(QtCore.QCoreApplication.translate("MainWindow", u"Refresh", None))
+    # retranslateUi
+
+if __name__ == "__main__":
+
+    app = QtWidgets.QApplication(sys.argv)
+    Window = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(Window)
+    Window.show()
+    app.exec_()
