9bbcf2d0ce90676799b62a3f27dfc3d65d01f5bf
Shuihan Liu <51645699+MaxLiu129@users.noreply.github.com>
2021-03-29 20:11:48 -0400

    add map click event

--- exampleDash.py
+++ exampleDash.py
@@ -6,6 +6,7 @@ import dash_html_components as html
 import dash_leaflet as dl
 import dash_leaflet.express as dlx
 from dash_extensions.javascript import Namespace
+from dash.dependencies import Input, Output
 
 n = 1440 * 5
 x = numpy.zeros(n)
@@ -41,12 +42,20 @@ points = [dict(lat=40.4259 + genwalk(i)[0], lon=-86.9081 + genwalk(i)[1], value=
 data = dlx.dicts_to_geojson(points)  
 # Create geojson.
 ns = Namespace("dlx", "scatter")
-geojson = dl.GeoJSON(data=data, options=dict(pointToLayer=ns("pointToLayer")))
+geojson = dl.GeoJSON(data=data, cluster=True, options=dict(pointToLayer=ns("pointToLayer")))
 # Create the app.
 app = dash.Dash(external_scripts=["https://cdnjs.cloudflare.com/ajax/libs/chroma-js/2.1.0/chroma.min.js"])
 app.layout = html.Div([
-    dl.Map([dl.TileLayer(), geojson], center=(40.4259, -86.9081), zoom=8, style={'height': '50vh'}),
+    dl.Map([dl.TileLayer(),
+    geojson, dl.LayerGroup(id="layer")], id="map",
+    center=(40.4259, -86.9081), zoom=8, style={'height': '50vh'}),
 ])
+
+# Add map click event.
+# once click the map, there will be a pin on the map shows the coordinates of the pin.
+@app.callback(Output("layer", "children"), [Input("map", "click_lat_lng")])
+def map_click(click_lat_lng):
+    return [dl.Marker(position=click_lat_lng, children=dl.Tooltip("({:.3f}, {:.3f})".format(*click_lat_lng)))]
 
 if __name__ == '__main__':
     app.run_server()
